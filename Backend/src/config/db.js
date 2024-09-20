c958c46aa11891d4c52e563d8496f92d6ee28aea
pradumkumar01 <pradumkumar7929@gmail.com>
2024-09-21 01:45:37 +0530

    change in this file

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const mongoDbUrl='mongodb+srv://pradumkumar7929:CKKePrJ847SAE3et@cluster0.rzg6del.mongodb.net/?retryWrites=true&w=majority'
+const mongoDbUrl='Enter Your url'
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
 }
